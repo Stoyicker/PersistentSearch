7d36241b5158bdded74ed2679cbeef2cdfc617ea
Jorge Antonio Díaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2015-05-20 10:04:00 +0200

    Fixes

--- persistentSearch/src/main/java/com/quinny898/library/persistentsearch/SearchBox.java
+++ persistentSearch/src/main/java/com/quinny898/library/persistentsearch/SearchBox.java
@@ -76,7 +76,6 @@ public class SearchBox extends RelativeLayout {
     private Fragment mContainerFragment;
     private android.support.v4.app.Fragment mContainerSupportFragment;
 
-
     /**
      * Create a new searchbox
      *
@@ -265,7 +264,6 @@ public class SearchBox extends RelativeLayout {
         } else {
             openSearch(Boolean.TRUE);
         }
-        searchOpen = !searchOpen;
     }
 
     /**
@@ -708,6 +706,7 @@ public class SearchBox extends RelativeLayout {
                     getApplicationWindowToken(),
                     InputMethodManager.SHOW_IMPLICIT, 0);
         }
+        searchOpen = Boolean.TRUE;
     }
 
     private void setInitialResults() {
@@ -742,6 +741,7 @@ public class SearchBox extends RelativeLayout {
                 .getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(getApplicationWindowToken(),
                 0);
+        searchOpen = Boolean.FALSE;
     }
 
     private void search(String text) {
